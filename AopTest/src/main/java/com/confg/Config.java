b3d8c2cd15d5b13af0402145fe2547b3054c9087
present43 <2351810431@qq.com>
2021-11-28 15:06:24 +0800

    配置切面驱动

--- AopTest/src/main/java/com/confg/Config.java
+++ AopTest/src/main/java/com/confg/Config.java
@@ -2,9 +2,11 @@ package com.confg;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan(basePackages = {"com.aop"})
+@EnableAspectJAutoProxy
 public class Config {
 
 
